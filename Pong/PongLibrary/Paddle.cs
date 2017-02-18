e0d44c7601fbf55f4c668a8701bf1c97ea1a6969
RoanWC <Roanwc@gmail.com>
2017-02-17 23:11:06 -0500

    %%%%%%%%WORKS%%%%%%%%%%%%%%%%%%%%%%%%%%%

--- Pong/PongLibrary/Paddle.cs
+++ Pong/PongLibrary/Paddle.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
-
+/// <summary>
+/// 
+/// </summary>
 namespace PongLibrary
 {
     public delegate void scoreIncreaseHandler(Paddle p);
